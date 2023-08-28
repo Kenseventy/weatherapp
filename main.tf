fdae0c4b557d244a3e8920e90dfbf3bc06a3bf07
Kenseventy <kmcomptech@gmail.com>
2023-08-28 10:02:14 -0500

    Added terraform extras and yml file

--- main.tf
+++ main.tf
@@ -16,8 +16,6 @@ resource "azurerm_container_registry" "acr" {
 }
 
 
-
-
 resource "azurerm_container_group" "aci" {
   name                = "weathercontainer"
   location            = azurerm_resource_group.rg.location
@@ -46,6 +44,47 @@ resource "azurerm_container_group" "aci" {
   tags = {
     environment = "az-204"
   }
+}
+
+# Create a storage account for the Function App
+resource "azurerm_storage_account" "funcstorage" {
+  name                     = "weathertracker"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create an App Service Plan for the Function App
+resource "azurerm_app_service_plan" "funcplan" {
+  name                = "EastUSLinuxDynamicPlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+# Create the Function App
+resource "azurerm_function_app" "funcapp" {
+  name                       = "weathertrackerfunction"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.funcplan.id
+  storage_account_name       = azurerm_storage_account.funcstorage.name
+  storage_account_access_key = azurerm_storage_account.funcstorage.primary_access_key
+  version                    = "~3" # Assuming you're targeting Functions v3. Adjust as needed.
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+  }
+}
+
+# Output for the Function App URL
+output "function_app_default_hostname" {
+  value = azurerm_function_app.funcapp.default_hostname
 }
 
 
